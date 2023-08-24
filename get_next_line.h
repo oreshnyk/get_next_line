07cc8b3604e21a9d8c574e2c955c9bbb7bba1143
Oleksii Reshetnyk <55344626+oreshnyk@users.noreply.github.com>
2023-08-24 18:48:11 +0200

    prep

--- get_next_line.h
+++ get_next_line.h
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <sys/type.h>
+# include <sys/uio.h>
+# include <unistd.h>
 
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n);
+char	*ft_strchr(const char *s, int c);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
